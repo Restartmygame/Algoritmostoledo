1cc7c5260eebe86144c0732eaa46e29c3532d46e
Diego Santos <sirdiego361@gmail.com>
2023-04-26 21:09:22 -0300

    atualização lista15
    foi feito o exercicio 4 mas não foi finalizado por bug desconhecido

---
+++ lista de exercicios/Exercicios C/exercicios/lista 15/Exercicio4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*Escreva um algoritmo que leia 2 números, após a leitura somar e exibir o resultado.*/
+
+void main(){
+	
+	int num1, num2, soma;
+
+	
+	
+	printf("Escreva o primeiro numero: ");
+	scanf("%i", &num1);
+	printf("Escreva o segundo numero: ");
+	scanf("%i", &num2);
+	
+	soma = num1 + num2;
+	
+	printf("%s","A soma dos numeros eh: ","%d", &soma);
+	
+	
+}
