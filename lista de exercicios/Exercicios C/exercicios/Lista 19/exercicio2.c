3023746d98e891cd4014be260a7f91b717bfcf83
Diego Santos <sirdiego361@gmail.com>
2023-06-13 03:01:06 -0300

    uptade exercicio 2
    Foi refeito o exercicio 2 e agora ele funciona corretamente

--- lista de exercicios/Exercicios C/exercicios/Lista 19/exercicio2.c
+++ lista de exercicios/Exercicios C/exercicios/Lista 19/exercicio2.c
@@ -7,36 +7,52 @@
 - Suponha que não exista dois ou mais alunos com a mesma nota.
 */
 
-int main(){
+void main(){
+     
+     int i, num, aluno_maior=0, aluno_segundo_maior=0;
+     float nota, nota_maior=0, segunda_maior=0;
 	
-	int matricula, quant, aluno_melhor = 0, aluno_seg_melhor = 0;
-	float nota, segunda_maior = 0, maior_nota = 0;
-	
-	for(quant=0; quant<7; quant++)
-		printf("Escreva seu numero de matricula: ");
-		scanf("%d", &matricula);
-		printf("Escreva sua nota: ");
-		scanf("%f", &nota);
-	
-	    //determina a maior nota
-                    if(nota > maior_nota){
-	                	maior_nota = nota;
-	                 	aluno_melhor = matricula;
-                   	}
-	
-	                //determina a segunda maior nota
-	                if(nota > segunda_maior && nota < maior_nota){
-	                	segunda_maior = nota;
-	                 	aluno_seg_melhor = matricula;
-                   	}
-	
-	
-	
-		
-	}
-	
-	printf("\n\nO aluno com a maior nota eh o numero %d com uma nota de %0.2f\n", aluno_melhor, maior_nota);
-	printf("O aluno com a segunda melhor nota eh o numero %d com uma nota de %0.2f", aluno_seg_melhor, segunda_maior);
-	
+	for(i=1;i<=7;i++){
+                      
+           printf("\naluno %d:\n",i);                
+           printf("escreva seu numero de matrucula: ");
+           scanf("%d",&num);
+           printf("escreva sua nota: ");
+           scanf("%f",&nota);
+           
+           if(i == 1){
+                nota_maior = nota;
+                segunda_maior = nota;
+                aluno_segundo_maior = num;
+                aluno_maior = num;
+                printf("teste\n");     
+           }
+           else{
+                 if(nota < nota_maior && nota > segunda_maior){
+                   segunda_maior = nota;
+                   aluno_segundo_maior = num;
+           }                 
+           
+                 if(nota > nota_maior){
+                   aluno_segundo_maior = aluno_maior;
+                   segunda_maior = nota_maior;
+                   nota_maior = nota;
+                   aluno_maior = num;
+                   
+                 }
+           }
+           
+           
+          
+    
+    
+    }
+    
+    printf("O aluno com a maior nota eh o numero %d com uma nota de %0.2f\n", aluno_maior, nota_maior);
+    printf("O aluno com a segunda maior nota eh o numero %d com uma nota de %0.2f", aluno_segundo_maior, segunda_maior);
+    
+    system("pause");
+    
+    return 0;
 	
 }
